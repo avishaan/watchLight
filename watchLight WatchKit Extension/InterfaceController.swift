63dcd759d77e77bb898bd12dc4eb2583e687cd70
Avishaan <avishaan@codeHatcher.com>
2015-07-28 02:30:02 -0700

    style WKGroup

--- watchLight WatchKit Extension/InterfaceController.swift
+++ watchLight WatchKit Extension/InterfaceController.swift
@@ -9,14 +9,18 @@
 import WatchKit
 import Foundation
 
+var isFlashLightOn = false
+
 
 class InterfaceController: WKInterfaceController {
   
+  @IBOutlet weak var button: WKInterfaceButton!
   @IBOutlet weak var interfaceGroup: WKInterfaceGroup!
   override func awakeWithContext(context: AnyObject?) {
     super.awakeWithContext(context)
     
     // Configure interface objects here.
+    interfaceGroup.setCornerRadius(0.0)
   }
   
   override func willActivate() {
@@ -29,6 +33,11 @@ class InterfaceController: WKInterfaceController {
     super.didDeactivate()
   }
   @IBAction func onButtonTap() {
+    if (!isFlashLightOn){
+      interfaceGroup.setBackgroundColor(UIColor.whiteColor())
+    } else {
+      interfaceGroup.setBackgroundColor(UIColor.clearColor())
+    }
   }
   
 }
