f014bd2fb73ecc3766cfce10c04ba7c18f866e3e
Avishaan <avishaan@codeHatcher.com>
2015-07-28 02:19:22 -0700

    basic controller interface setup

---
+++ watchLight WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,34 @@
+//
+//  InterfaceController.swift
+//  watchLight WatchKit Extension
+//
+//  Created by Brown Magic on 7/28/15.
+//  Copyright (c) 2015 codeHatcher. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+  
+  @IBOutlet weak var interfaceGroup: WKInterfaceGroup!
+  override func awakeWithContext(context: AnyObject?) {
+    super.awakeWithContext(context)
+    
+    // Configure interface objects here.
+  }
+  
+  override func willActivate() {
+    // This method is called when watch view controller is about to be visible to user
+    super.willActivate()
+  }
+  
+  override func didDeactivate() {
+    // This method is called when watch view controller is no longer visible
+    super.didDeactivate()
+  }
+  @IBAction func onButtonTap() {
+  }
+  
+}
